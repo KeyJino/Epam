2135988bfc84a9d9bfe694d67f85cb4b9a7c152e
KeyJino <jeka.kova@mail.ru>
2017-10-26 01:00:58 +0300

    initial commit

---
+++ checkpoint_1/Testing/Testing/facade/bins/AShooes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bin
+{
+    public abstract class AShooes
+    {
+        private int size;
+        private int price;
+        private int materialId;
+
+        public AShooes(int size, int price, int materialId)
+        {
+            this.price = price;
+            this.size = size;
+            this.materialId = materialId;
+        }
+
+        public int getSize()
+        {
+            return size;
+        }
+
+        public int getPrice()
+        {
+            return price;
+        }
+
+        public int getMaterialID()
+        {
+            return materialId;
+        }
+
+
+    }
+}

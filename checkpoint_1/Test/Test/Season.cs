d5953e755e9038f01958e3064f9760056adea737
KeyJino <jeka.kova@mail.ru>
2017-10-27 12:10:36 +0300

    Fix #2
    Reproject
    1. Add new classses;
    2. Add new method.

---
+++ checkpoint_1/Test/Test/Season.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Season : AShooes
+    {
+        public Season(int size, int price, int materialId) : base(size, price, materialId)
+        {
+            
+        }
+    }
+}
